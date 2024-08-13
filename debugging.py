0e488faf7e91a93860748929f1d1c4701743ea3c
swayam singal <132289274+swayam8624@users.noreply.github.com>
2024-08-13 20:41:01 +0530

    Add files via upload

---
+++ debugging.py
@@ -0,0 +1,23 @@
+#debugging 
+
+#linting
+
+#ide/editor
+
+#read errors
+
+#pdb [very useful]
+import pdb
+
+def add(num1, num2):
+    pdb.set_trace()
+    return num1+num2
+
+print(add(4,'sds'))
+
+#help to get the doc about things
+#step to go to next step
+# a gives all the arguments and values
+# we can also change variable values
+
+
